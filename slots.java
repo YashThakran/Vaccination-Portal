5acc48909ed16dd4f90d53d172a0f588eb66e658
Yash Thakran <77500759+YashThakran@users.noreply.github.com>
2022-01-27 23:09:01 +0530

    Add files via upload

---
+++ slots.java
@@ -0,0 +1,13 @@
+package assignment1;
+public class slots {
+    String id_hospital;
+    int qty;
+    String vacc;
+    int day;
+    slots(String id_hospital, int qty, String vacc,int day){
+        this.id_hospital = id_hospital;
+        this.qty = qty;
+        this.day = day;
+        this.vacc = vacc;
+    }
+}
