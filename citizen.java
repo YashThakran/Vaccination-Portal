8dc8e5b4ff5ea00a6b0f54df040c1f88c911d1b6
Mohit Bansal <88699911+mohitbansal2020526@users.noreply.github.com>
2022-01-27 22:44:20 +0530

    Add files via upload

---
+++ citizen.java
@@ -0,0 +1,21 @@
+package assignment1;
+public class citizen {
+    String name;
+    int Age;
+    String ID_citizen;
+    String status;
+    int day_of_vaccination;
+    int due_date;
+    String vaccine;
+    int doses_given;
+    int total_doses;
+    citizen(String name ,int Age ,String ID_citizen){
+        this.name = name;
+        this.Age = Age;
+        this.ID_citizen = ID_citizen;
+        this.status = "REGISTERED";
+        doses_given=0;
+        total_doses=0;
+        due_date = -1;
+    }
+}
