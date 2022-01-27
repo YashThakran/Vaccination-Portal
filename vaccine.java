b9dca5998dcaa2e443733f52dbad5c58fff4073a
Yash Thakran <77500759+YashThakran@users.noreply.github.com>
2022-01-27 23:09:35 +0530

    Add files via upload

---
+++ vaccine.java
@@ -0,0 +1,11 @@
+package assignment1;
+public class vaccine {
+    String name;
+    int no_doses;
+    int gap_doses;
+    vaccine(String nam, int doses, int gap){
+        this.name = nam;
+        this.no_doses = doses;
+        this.gap_doses = gap;
+    }
+}
